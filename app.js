1a7974906b323d4d0ff401eaca3274d0f9c1c6a9
藤井沙綾 <say.f@fujiiisagoryounoMacBook-Pro.local>
2020-10-08 17:58:51 +0900

    [Finish]javascript2

--- app.js
+++ app.js
@@ -1,19 +1,5 @@
-var max = 100;
-var num = 1;
-var count = 0;
+var num1 = 5;
+var num2 = 6;
+var count = (num1/num2);
 
-while(num < max){
-  num = num * 2;
-  count = count + 1;
-}
-
-alert('２を掛け続けて' + max + 'を超えるのに必要だった回数は' + count + '回です');
-
-var i;
-var num = 0;
-
-for (i = 1; i < 11; i++){
-  num = num + i;
-}
-
-alert('1から10まで足算した結果は' + num + 'です');
+alert(num1 + '÷' + num2 + '=' + count);
